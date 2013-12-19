a653ef02996c98a4a1e6cccdf7507033260a9fe1
Fede Isas <fedeisas@hotmail.com>
2013-12-19 20:16:23 -0300

    initial commit

---
+++ app/config/production/database.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'connections' => array(
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'cuantocuesta.mysql.eu1.frbit.com',
+            'database'  => 'cuantocuesta',
+            'username'  => 'cuantocuesta',
+            'password'  => 'XUk4tA1JpU_l83Oo',
+        )
+    )
+);

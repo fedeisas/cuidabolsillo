0cc43876f605961028bfe69684c448bd987df8a6
Fede Isas <fedeisas@hotmail.com>
2013-12-20 21:28:45 -0300

    travis env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('atlantis'),
-    'testing' => array('atlantis'),
+    'testing' => array('*travis*'),
     'production' => array('web302.webfaction.com'),
 
 ));

0cd038e6b8665d0acf2114bc05135114bccd2655
Fede Isas <fedeisas@hotmail.com>
2013-12-20 23:05:10 -0300

    ios bug

--- app/views/partials/meta.blade.php
+++ app/views/partials/meta.blade.php
@@ -1,3 +1,5 @@
+<base href="{{{ route('home') }}}">
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
@@ -6,9 +8,9 @@
 <meta name="author" content="Fede Isas, Guillermo Mazzucco, Gabriel Maza">
 
 <meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
 <meta name="mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-title" content="{{{ Config::get('custom.name') }}}">
+<meta name="apple-mobile-web-app-title" content="CuidaBolsillo">
 <meta name="format-detection" content="telephone=no">
 
 <title>{{{ $title }}}</title>
